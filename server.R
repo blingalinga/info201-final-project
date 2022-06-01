8a63b30e6e296f125b45fee75a626ee5a40e77b4
blingalinga <ling10@uw.edu>
2022-05-31 22:34:53 -0700

    structure setup

--- server.R
+++ server.R
@@ -1,1 +1,23 @@
+library(ggplot2)
+library(plotly)
+library(dplyr)
+
+climate_df <- read.csv("https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.csv", stringsAsFactors = FALSE)
+
+server <- function(input, output) {
+  
+  output$climate_plot <- renderPlotly({
+    
+    filtered_df <- climate_df %>% 
+      filter(country %in% input$country_selection) %>% 
+      filter(year >= input$slider1[1] & year <= input$slider1[2])
+    
+    co2_plot <- ggplot(data = filtered_df) +
+      geom_line(mapping = aes(x = year, y = co2, color = country)) +
+      labs(title = "CO2 Trends", x = "Year", y = "CO2 Emissions")
+    
+    return(co2_plot)
+  })
+  
+}
 
--- ui.R
+++ ui.R
@@ -1,1 +1,66 @@
+library(plotly)
+library(bslib)
+
+# Load climate data
+climate_df <- read.csv("https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.csv", stringsAsFactors = FALSE)
+
+# Home page tab
+intro_tab <- tabPanel(
+  # Title of tab
+  "Introduction",
+  fluidPage(
+    theme = bs_theme(version = 4, bootswatch = "minty"),
+    h1("Let's Talk about Climate Change"),
+    p("Filler Text"),
+    p("Info about data"),
+    h2("Summary Calculations"),
+    p("Write summary calculations here"), 
+    p("More information")
+  )
+)
+
+# We want our next tab to have a sidebar layout
+# So we're going to create a sidebarPanel() and a mainPanel() and then add them together
+
+# Create sidebar panel for widget
+sidebar_panel_widget <- sidebarPanel(
+  checkboxGroupInput(
+    inputId = "country_selection",
+    label = h4("Select a Region"),
+    choices = c("World", "United States", "China", "Asia"),
+    selected = "United States"
+  ),
+  sliderInput(inputId = "slider1",
+              label = h4("Select Year Range"),
+              min = min(climate_df$year),
+              max = max(climate_df$year),
+              sep = "",
+              value = c(2010, 2020)
+  )
+)
+
+# Put a plot in the middle of the page
+main_panel_plot <- mainPanel(
+  plotlyOutput(outputId = "climate_plot"),
+  fluidPage(
+    p("Filler Text")
+  )
+)
+
+# Climate tab  — combine sidebar panel and main panel
+climate_tab <- tabPanel(
+  "Climate Viz",
+  sidebarLayout(
+    sidebar_panel_widget,
+    main_panel_plot
+  )
+)
+
+
+ui <- navbarPage(
+  # Home page title
+  "Climate Change",
+  intro_tab,
+  climate_tab
+)
 
