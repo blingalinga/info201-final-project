87e4990e4f42aa55d4b247f349e2f5a47b60dc6c
Christopher James Johnson <cj444@uw.edu>
2022-06-01 20:37:10 -0700

    Fixed non-numeric argument error

--- server.R
+++ server.R
@@ -8,66 +8,66 @@ climate_df <- read.csv("https://datahub.io/core/co2-ppm/r/0.csv", stringsAsFacto
 server <- function(input, output) {
   
   output$climate_plot1 <- renderPlotly({
-    
-    filtered_df <- climate_df %>% 
-      mutate(DateConverted = as.Date(Date)) %>% 
+
+    filtered_df <- climate_df %>%
+      mutate(DateConverted = as.Date(Date)) %>%
       filter(DateConverted >= input$slider1[1] & DateConverted <= input$slider1[2])
-    
+
     co2_plot1 <- ggplot(data = filtered_df) +
       geom_line(mapping = aes(x = DateConverted, y = Average)) +
       labs(title = "CO2 EmissionTrends", x = "Date", y = "Average CO2 Emissions")
-    
+
     return(co2_plot1)
   })
-  
+#   
   output$climate_plot2 <- renderPlotly({
-    
-    climate_df_mean <- climate_df %>% mutate(Year = str_sub(string = Date, 1, 4)) %>% 
+
+    climate_df_mean <- climate_df %>% mutate(Year = str_sub(string = Date, 1, 4)) %>%
       filter(Average != -99.99 & Year > 1958) %>% group_by(Year) %>% summarize(mean_co2 = mean(Average))
-    
+
     climate_df_mean <- climate_df_mean %>% summarize(annual_change = mean_co2 - lag(mean_co2))
-    
+
     co2_plot2 <- ggplot(data = filtered_df) +
       geom_line(mapping = aes(x = year, y = change)) +
       labs(title = "CO2 Trends", x = "Year", y = "Annual Change in CO2 Emissions")
-    
+
     return(co2_plot2)
   })
-  
+#   
   output$climate_plot3 <- renderPlotly({
-    
-    filtered_df <- climate_df %>% 
-      mutate(DateConverted = as.Date(Date)) %>% 
+
+    filtered_df <- climate_df %>%
+      mutate(DateConverted = as.Date(Date)) %>%
       filter(DateConverted >= input$slider3[1] & DateConverted <= input$slider3[2])
-    
+
     co2_plot3 <- ggplot(data = filtered_df) +
       geom_line(mapping = aes(x = DateConverted, y = Average)) +
       labs(title = "CO2 Trends", x = "Date", y = "CO2 Emissions")
-    
+
     return(co2_plot3)
   })
 
-<<<<<<< HEAD
-  
-    
-    henry_co2data <- climate_df %>% 
-      filter(Number.of.Days > -1) %>% 
-      filter(Date >= input$slider1[1] & Date <= input$slider1[2])
-   
+# <<<<<<< HEAD
+
+
+
+
     output$climateplot4 <- renderPlotly({
       
-   co2_plot4 <- ggplotly(
-     ggplot(data = henry_co2data) +
-       geom_point(mapping = aes(x = Date, 
+      henry_co2data <- climate_df %>%
+        filter(Number.of.Days > -1) %>%
+        filter(Date >= input$slider1[1] & Date <= input$slider1[2])
+
+   co2_plot4 <- ggplot(data = henry_co2data) +
+       geom_point(mapping = aes(x = Date,
                                y = Number.of.Days,
                                color = Average)) +
       labs(title = "Number of Daily Readings Contributed in Each Month",
            x = "Month",
-           y= "Number of Readings",
-           color = "Number of Days"))
+           y= "Number of Readings")
    return(co2_plot4)
   })
 }
-=======
-%
->>>>>>> 285a96e539320d1e7b6719f33135b3a663b1acbe
+# =======
+# %
+# >>>>>>> 285a96e539320d1e7b6719f33135b3a663b1acbe
